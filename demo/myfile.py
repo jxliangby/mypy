fddf0d4aeb41ec5fe77e8f279a2183df389bffe2
rokcy jiang <kissjava.rocky@gmail.com>
2016-05-31 22:47:11 +0800

    is

--- demo/myfile.py
+++ demo/myfile.py
@@ -1,9 +1,10 @@
 import os
 import glob
+import time
 from fnmatch import fnmatch
 
 def fileread(path,logpath):
-	with open(logpath, 'wt') as log:
+	with open(logpath, 'a') as log:
 		print("open file path:%s" %(path),file=log)
 		with open(path, 'rt') as f:
 			for line in f:
@@ -11,7 +12,9 @@ def fileread(path,logpath):
 			
 def findfiles(path):
 	pys = glob.glob(path+"*.py")
-	print(pys)
+	name_sz_date = [(name,os.path.getsize(name),os.path.getmtime(name)) for name in pys]
+	for name,size,mtime in name_sz_date:
+		print(name,size, time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(mtime)))
 	
 	pys2 = [name for name in os.listdir(path) if fnmatch(name, "*.py")]
 	print(pys2)
@@ -19,6 +22,6 @@ def findfiles(path):
 rootPath = "d:\\workspace\\python\\mypy\\demo\\"
 logpath = rootPath + "log.txt"
 
-#fileread(rootPath+"01.py",logpath)
-findfiles(rootPath)
+fileread(rootPath+"myfile.py",logpath)
+#findfiles(rootPath)
 	
